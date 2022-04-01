a014a11e7c7308bd9d347013550a2cd23336c73a
Ricky Mormor <rickymormor1@gmail.com>
2022-04-01 02:39:27 +0100

    Fix routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
 class UsersController < ApplicationController
-  def new
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  #get 'users'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
-  resources :users, only: [:new, :show] do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
 end
