0d09e438191319e17d1bb6117f318d3c4fe77eff
Ricky Mormor <rickymormor1@gmail.com>
2022-04-02 08:05:22 +0100

    generate users, posts and comments migration

--- db/migrate/20220401131950_create_posts.rb
+++ db/migrate/20220401131950_create_posts.rb
@@ -1,6 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.integer :commets_counter, default: 0
+      t.integer :likes_counter, default: 0
 
       t.timestamps
     end
