e2b42f63eda33c2b90574ce8839a9acdc8e3a87e
Ricky Mormor <rickymormor1@gmail.com>
2022-04-13 23:12:34 +0100

    Add views for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
