f7f1f84fd564d55a071c307b4a37af5df84aee04
Ricky Mormor <rickymormor1@gmail.com>
2022-04-14 12:16:59 +0100

    Add posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,13 @@
 class PostsController < ApplicationController
-  def index; end
-  def show; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments.order(created_at: :desc)
+    @comment = Comment.new
+    @like = Like.new
+  end
 end
