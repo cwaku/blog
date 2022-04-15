9e0b14ea4b68da41045b19f10c7e351ca8ad3e31
Ricky Mormor <rickymormor1@gmail.com>
2022-04-15 14:12:30 +0100

    Move logic from model to controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,12 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @new_comment = current_user.comments.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @new_comment = current_user.comments.new(
+      post_id: @post.id,
+      author_id: current_user.id,
+      text: comment_params[:text]
+    )
     respond_to do |format|
       format.html do
         if @new_comment.save
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,24 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def new
-    @comment = Comment.new
-  end
-
-  def create
-    @post = Post.find(params[:post_id])
-    @comment = current_user.comments.build(
-      post_id: @post.id,
-      author_id: current_user.id,
-      text: comment_params[:text]
-    )
-    if @comment.save
-      redirect_to post_path(@post), notice: 'Comment was successfully created.'
-    else
-      redirect_to post_path(@post), notice: 'Comment was not created.'
-    end
-  end
-
   private
 
   def update_comment_counter
