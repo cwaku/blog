98bb5a72cd1504816579e929d0aaf47f97500738
Ricky Mormor <rickymormor1@gmail.com>
2022-04-16 11:03:59 +0100

    Add tests for user likes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'The post' do
+    first_user = User.create(name: 'Ricky', photo: 'pic.jpg', bio: 'I am a test user')
+    first_post = Post.create(title: 'Test Post', text: 'This is a test post', author: first_user)
+    like = Like.new(author: first_user, post: first_post)
+
+    like.save!
+
+    it 'should have likes counter' do
+      expect(first_post.likes.count).to eq(1)
+    end
+  end
 end
