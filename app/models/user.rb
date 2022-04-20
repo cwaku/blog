2f95d025a8360ae473f7318c5d50dee05c3534ed
Ricky Mormor <rickymormor1@gmail.com>
2022-04-20 15:28:07 +0100

    Add role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,10 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def admin?
+    role == 'admin'
+  end
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
   end
