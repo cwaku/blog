2b5347eca1222e5c445517eca87bfc49e3c00088
Ricky Mormor <rickymormor1@gmail.com>
2022-04-21 20:29:01 +0100

    Make all model and request tests pass

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,22 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'The comments model' do
-    first_user = User.create(name: 'Ricky', photo: 'pic.jpg', bio: 'I am a test user')
-    first_post = Post.create(title: 'Test Post', text: 'This is a test post', author: first_user)
-    first_comment = Comment.create(text: 'This is a test comment', author: first_user, post: first_post)
-
-    first_comment.save
-
-    it 'should have a text' do
-      expect(first_comment.text).to eq('This is a test comment')
+    before(:each) do
+      first_user = User.create!(name: 'Ricky', photo: 'photo.jpg', bio: 'Alien from Earth 300.',
+                                email: 'suur@example.com', password: 'password')
+      first_post = Post.create!(author_id: first_user.id, title: 'Hello', text: 'This is my first visit to Mars.')
+      Comment.create!(post_id: first_post.id, author_id: first_user.id, text: 'Beep Bop!')
     end
 
-    it 'should have an author' do
-      expect(first_comment.author).to eq(first_user)
+    it 'should have a text' do
+      expect(Comment.first.text).to eq 'Beep Bop!'
     end
 
     it 'comment must be present' do
-      expect(first_post.comments.count).to eq(1)
+      expect(Comment.all.length).to eq 1
     end
   end
 end
