d19b83754b9c9b15290de20741e484c1059fabde
Ricky Mormor <rickymormor1@gmail.com>
2022-04-22 16:48:00 +0100

    Setup

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show, :create, :update, :destroy]
+      resources :posts, only: [:index, :show, :create, :update, :destroy]
+      resources :comments, only: [:index, :show, :create, :update, :destroy]
+      resources :likes, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: %i[index show create new destroy] do
       resources :comments, only: %i[create destroy]
